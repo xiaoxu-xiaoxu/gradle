8dad4cb485b25700df013522cf7447efc00c690b
xuzhendong <team2@jdd.com>
2024-01-25 16:33:19 +0800

    1

---
+++ gradle-spring1/src/main/java/com/xiaoxu/spring1/GradleSpring1Application.java
@@ -0,0 +1,14 @@
+package com.xiaoxu.spring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSpring1Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(GradleSpring1Application.class, args);
+    }
+
+}
---
+++ gradle-spring2/src/main/java/com/xiaoxu/gradlespring2/GradleSpring2Application.java
@@ -0,0 +1,13 @@
+package com.xiaoxu.gradlespring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSpring2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleSpring2Application.class, args);
+    }
+
+}
